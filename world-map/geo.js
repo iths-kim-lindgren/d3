b56c0c0547f777a338955f304c89eee6cc61f289
iths-kim-lindgren <48940807+iths-kim-lindgren@users.noreply.github.com>
2020-08-13 16:02:55 +0200

    playable game

--- world-map/geo.js
+++ world-map/geo.js
@@ -1,11 +1,11 @@
-// var topology = topojson.topology({foo: geojson});
+
 
 // const data = "./world.json"
 
 (async function () {
-    var margin = { top: 50, left: 50, right: 50, bottom: 50 },
-        height = 400 - margin.top - margin.bottom,
-        width = 800 - margin.left - margin.right;
+    var margin = { top: 2000, left: 50, right: 50, bottom: 50 },
+        height = 800 - margin.top - margin.bottom,
+        width = 1200 - margin.left - margin.right;
 
     var svg = d3.select("#map")
         .append("svg")
@@ -15,21 +15,36 @@
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     // PROJECTION
-    var projection = d3.geoMercator()
+    var projection = d3.geoEqualEarth()
         .translate([width / 2, height / 2])
-        .scale(100)
+        .scale(1000)
+    // center()
+
+    // geocentroid
+
+    console.log(d3)
 
     // CREATE PATH
 
     var path = d3.geoPath()
         .projection(projection)
 
+        var time = 0
+        var countUp = setInterval(function (){
+            time++
+            document.querySelector("h3").innerText = `Time: ${time} seconds`
+        }, 1000)
 
-    const data = await d3.json("world.json")
+    const data = await d3.json("europe.geo.json")
 
+    var topology = topojson.topology({ foo: data });
 
-    const countries = topojson.feature(data, data.objects.ne_110m_admin_0_countries1).features
+    const countries = topojson.feature(topology, topology.objects.foo).features
+    // const countries = topojson.feature(data, data.objects.ne_110m_admin_0_countries1).features
     console.log(countries)
+    let targetCountries = countries
+    selectCountry()
+
 
     svg.selectAll(".country")
         .data(countries) /* binder selectAll till enter() */
@@ -44,9 +59,34 @@
         })
         .on("click", function (d) {
             d3.selectAll(".country")
-            .classed("selected", false)
+                .classed("selected", false)
+                if (d.targetCountry && !d.previousTarget){
+                    let index = targetCountries.indexOf(d)
+                    d.previousTarget = true
+
+                    targetCountries.splice(index, 1)
+                    selectCountry()
+                }
             d3.select(this).classed("selected", true)
         })
+        
+        function selectCountry() {
+            if (targetCountries.length == 0) {
+                document.querySelector("h2").innerText = `Solved it in ${time} seconds!`
+                clearInterval(countUp)
+           return
+        }
+        let rand = Math.floor(Math.random() * targetCountries.length)
+        console.log(targetCountries)
+        for (let country in targetCountries) {
+            targetCountries[country].targetCountry = false
+            if (country == rand) {
+                targetCountries[country].targetCountry = true
+                document.querySelector("h2").innerText = `Click on ${targetCountries[country].properties.admin}`
+            }
+        }
+    }
+
 
 
 
